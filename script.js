1750ff54f19e319ce4bde16555944179a0e01ff7
Kristine <kristinevt2004@gmail.com>
2024-07-24 15:49:28 +0530

    First Day

---
+++ script.js
@@ -0,0 +1,36 @@
+//const button= document.getElementById('eventbtn');
+//const text= document.getElementById('text');
+
+//button.addEventListener('click', function(){
+//   text.innerHTML='This is a changed text'
+//});
+
+// const persons={
+//     firstName:"Kevin",
+//     lastName:"Doe",
+//     age:22,
+//     eyeColor:'blue'
+// }
+
+// console.log(persons.firstName+" "+"is"+" "+persons.age+" years old")
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log("Looping through"+i);
+    
+// }
+
+// function square(number)
+// {
+//     return number*number;
+// }
+// console.log(square(2));
+
+// let square=(number)=>{
+//     return number*number;
+// }
+// console.log(square(2));
+
+let sum=(num1,num2)=>{
+    return num1+num2;
+}
+console.log(sum(2,4));
